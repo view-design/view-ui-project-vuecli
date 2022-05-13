52089f3c919022e790bb4b4fa70547d00e50f4a4
Hunter <273676636@qq.com>
2022-05-13 14:23:22 +0800

    update dependencies

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import ViewUIPlus from 'view-ui-plus'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import 'view-ui-plus/dist/styles/viewuiplus.css'
 import './styles/index.less'
 // import './mock'
 
